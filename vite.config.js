dd4c005493214e4f4d1e5045dc140ac3b1e731ad
catmint4a <catmint_4a@hotmail.com>
2025-06-29 17:33:23 +0900

    Update build configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,8 @@ export default defineConfig({
   },
   base: '/pomodoro/',
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       input: {
         main: './index.html'
