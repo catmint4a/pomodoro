1e2bb314319874af6e8d31cc5bf13fa4425c6749
catmint4a <catmint_4a@hotmail.com>
2025-06-29 18:16:16 +0900

    Update script loading configuration

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './index.html'
-      },
-      output: {
-        entryFileNames: `assets/[name]-[hash].js`,
-        chunkFileNames: `assets/[name]-[hash].js`,
-        assetFileNames: `assets/[name]-[hash].[ext]`
       }
     }
   }
