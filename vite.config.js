ee1cf0b44bba72b92c11f1b6c1aeb70254c76ee2
catmint4a <catmint_4a@hotmail.com>
2025-06-29 19:54:10 +0900

    Update build configuration and fix script path

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
         main: './index.html'
       },
       output: {
-        entryFileNames: '[name]-[hash].js',
+        entryFileNames: 'index-[hash].js',
         chunkFileNames: '[name]-[hash].js',
         assetFileNames: '[name]-[hash].[ext]'
       }
