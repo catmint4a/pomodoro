5c709ec1033e3e3867f8a09846e31bb055ed936a
catmint4a <catmint_4a@hotmail.com>
2025-06-29 17:43:27 +0900

    Update build configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/pomodoro/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
+    assetsDir: 'assets'
   }
 })
