da3c3a13ebc384c23dc1162ea8d96bfcda4cead9
catmint4a <catmint_4a@hotmail.com>
2025-06-29 19:59:35 +0900

    fix for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/pomodoro/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      },
-      output: {
-        entryFileNames: 'index-[hash].js',
-        chunkFileNames: '[name]-[hash].js',
-        assetFileNames: '[name]-[hash].[ext]'
-      }
-    },
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true
-      }
-    }
-  }
 })
