caf4cb509920479d31485ecfbc01b035bb17e585
catmint4a <catmint_4a@hotmail.com>
2025-06-29 20:31:18 +0900

    use docs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,28 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true
+      }
+    },
+    manifest: true,
+    emptyOutDir: true,
+    sourcemap: true,
+    assetsInlineLimit: 0,
+    assetsInlinePattern: '\\.(png|jpe?g|gif|svg)$'
+  },
   base: '/pomodoro/',
+  server: {
+    base: '/pomodoro/'
+  }
 })
