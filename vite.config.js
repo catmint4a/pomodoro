f067fa4a2f563258017c8f8b52985597d52b5348
catmint4a <catmint_4a@hotmail.com>
2025-06-29 21:16:36 +0900

    fix vite.config 8

--- vite.config.js
+++ vite.config.js
@@ -23,8 +23,8 @@ export default defineConfig({
     assetsInlineLimit: 0,
     assetsInlinePattern: '\\.(png|jpe?g|gif|svg)$'
   },
-  base: '/pomodoro',
+  base: 'pomodoro',
   server: {
-    base: '/pomodoro'
+    base: 'pomodoro'
   }
 })
